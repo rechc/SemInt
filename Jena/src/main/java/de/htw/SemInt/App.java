9ce608410625c5827d6790f4d4c4d2f838b29808
rechc <rech.christian@gmail.com>
2012-06-15 19:15:55 +0200

    implmended example code from jena tutorial

--- Jena/src/main/java/de/htw/SemInt/App.java
+++ Jena/src/main/java/de/htw/SemInt/App.java
@@ -1,19 +1,25 @@
 package de.htw.SemInt;
 
 
+import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.Resource;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
     	Model m = ModelFactory.createDefaultModel();
+    	String NS = "http://example.com/test/";
     	
-        System.out.println( "Hello World!" );
+    	Resource r = m.createResource(NS + "r");
+    	Property p = m.createProperty(NS + "p");
+    	
+    	r.addProperty(p,  "hello world", XSDDatatype.XSDstring);
+    	
+    	m.write(System.out, "Turtle");
     }
 }
