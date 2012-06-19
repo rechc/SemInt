f0dc483a31d417852011dd1803d4a7532313ebd1
Christian Rech <rech.christian@googlemail.com>
2012-06-19 12:29:48 +0200

    extended onto exmple

--- Jena/src/main/java/de/htw/SemInt/OntologyTest.java
+++ Jena/src/main/java/de/htw/SemInt/OntologyTest.java
@@ -1,33 +1,70 @@
 package de.htw.SemInt;
 
+import com.hp.hpl.jena.ontology.OntClass;
+import java.io.FileNotFoundException;
 import java.util.Iterator;
 
 import com.hp.hpl.jena.util.iterator.Filter;
 import com.hp.hpl.jena.ontology.OntModel;
 import com.hp.hpl.jena.ontology.OntModelSpec;
+import com.hp.hpl.jena.rdf.model.InfModel;
+import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.reasoner.Reasoner;
 import com.hp.hpl.jena.reasoner.ReasonerRegistry;
+import com.hp.hpl.jena.util.PrintUtil;
+import java.io.FileInputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class OntologyTest {
 
 	public static void main(String[] args) {
-		OntModel onto = ModelFactory.createOntologyModel(
-				OntModelSpec.OWL_MEM, null);
-		
-		onto.read( "file:art.n3", "N3" );
-		
-//		Iterator i = onto.listHierarchyRootClasses()
-//		        .filterDrop( new Filter() {
-//		            public boolean accept( Object o ) {
-//		            return ((Resource) o).isAnon();
-//		            }} );
-//
-//		while (i.hasNext()) {
-//		    System.out.println(i.next().toString());
-//		}
-		
-		Reasoner reasoner = ReasonerRegistry.getOWLMiniReasoner();
+            String ns = "http://www.semanticweb.org/bline_asus/ontologies/2012/5/untitled-ontology-5#";
+            String ontopath = "./onto.owl";
+            
+             try {
+                OntModel onto = ModelFactory.createOntologyModel(
+                                OntModelSpec.OWL_MEM, null);
+
+                //onto.read( "file:art.n3", "N3" );
+                onto.read((new FileInputStream(ontopath)), "RDF/XML");
+
+                Iterator i = onto.listHierarchyRootClasses()
+                        .filterDrop( new Filter() {
+                            public boolean accept( Object o ) {
+                            return ((Resource) o).isAnon();
+                            }} );
+
+                while (i.hasNext()) {
+                    System.out.println(i.next().toString());
+                }
+                
+                OntClass sm = onto.getOntClass(ns + "Smartphone");
+                System.out.println("Equivalent Class: " + sm.getEquivalentClass());
+                 
+                Reasoner reasoner = ReasonerRegistry.getOWLMiniReasoner();
+                reasoner = reasoner.bindSchema (onto);
+                
+                //To use that together with instance data we have to load that into a separate model:
+                Model instances = ModelFactory.createDefaultModel();
+                instances.read ((new FileInputStream(ontopath)), "RDF/XML");
+                
+                InfModel model = ModelFactory.createInfModel (reasoner, instances);
+                Resource r = model.getResource (ns + "Kunde");
+                for (StmtIterator sti = model.listStatements(r, null, (RDFNode) null);
+                     sti.hasNext(); ) {
+                     Statement stmt = sti.nextStatement();
+                     System.out.println(" - " + PrintUtil.print(stmt));
+                }
+                
+            } catch (FileNotFoundException ex) {
+                //Logger.getLogger(OntologyTest.class.getName()).log(Level.SEVERE, null, ex);
+            }
 	}
 	
 }
--- Jena/src/main/java/de/htw/SemInt/ReadModel.java
+++ Jena/src/main/java/de/htw/SemInt/ReadModel.java
@@ -1,5 +1,7 @@
 package de.htw.SemInt;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.Property;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Iterator;
@@ -19,24 +21,33 @@ import com.hp.hpl.jena.vocabulary.VCARD;
 public class ReadModel {
 
 	public static void main(String[] args) {
-		String ns = "onto.owl#";
+            String ns = "http://www.semanticweb.org/bline_asus/ontologies/2012/5/untitled-ontology-5#";
 	    String xmlbase = "http://www.soctrace.org/ontologies/#";
-	    String ontopath = ".\\onto.owl";
+	    String ontopath = "./onto.owl";
 	    //String ontopath = "Z:\\workspace\\SI_Projekt\\SemInt\\Jena\\onto.owl";
 		
-	    try {
-	    	OntModel model = ModelFactory.createOntologyModel(ProfileRegistry.OWL_DL_LANG);
+               try {
+                    OntModel model = ModelFactory.createOntologyModel(ProfileRegistry.OWL_DL_LANG);
 
-	    	//Jena does not support OWL/XML format
-	    	//RDF/XML format ist needed
-			model.read((new FileInputStream(ontopath)), "RDF/XML");
-			model.write(System.out);
-			
-//			OntClass c1 = model.getOntClass(ns + "Smartphone");
-//			for (Iterator i = c1.listSubClasses(); i.hasNext();){
-//				OntClass c = (OntClass) i.next();
-//				System.out.println(c.getURI());
-//			}
+                    //Jena does not support OWL/XML format
+                    //RDF/XML format ist needed
+                    model.read((new FileInputStream(ontopath)), "RDF/XML");
+                    model.write(System.out);
+
+                    System.out.println("Zugriff auf Model: Get Smartphone classes");
+                    OntClass c1 = model.getOntClass(ns + "Smartphone");
+                    //OntClass c1 = model.getOntClass(ns + "SpieleSmartphone");
+                    for (Iterator i = c1.listSubClasses(); i.hasNext();){
+                            OntClass c = (OntClass) i.next();
+                            System.out.println(c.getURI());
+                    }
+
+                    Resource r = model.getResource(ns + "Smartphone");
+                    System.out.println("Res: " + r.toString());
+                    Property property = model.getProperty(ns + "hatEigenschaft");
+                    //System.out.println("isProp:" +   property.isProperty());
+                    //System.out.println("URI:" + property.getURI());
+                    System.out.println("Prop: " + r.getProperty(property));
 		} catch (FileNotFoundException e) {
 			System.out.println("File not found");
 			//e.printStackTrace();
